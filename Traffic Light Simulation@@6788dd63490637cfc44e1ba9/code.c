effd09fe1f9a9690453201e0b31621935b607edf
Meghna Rana <meghna1870.becse24@chitkara.edu.in>
2025-02-13 22:20:18 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char grade;
+    scanf("%c",&grade);
+    if (grade=='R'){
+        printf("Stop");
+    }else if (grade=='G'){
+        printf("Go");
+    }else if (grade=='Y'){
+        printf("Slow Down");
+    else{
+        printf("Invalid input");
+    }
+}
